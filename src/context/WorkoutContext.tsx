cb4d05ae8bcfb18380e4196c34ad7bccd522c275
cynix7 <tanvirwd007@gmail.com>
2026-09-26 17:20:31 +0600

    Type-issue fixed

--- src/context/WorkoutContext.tsx
+++ src/context/WorkoutContext.tsx
@@ -1,11 +1,26 @@
 'use client';
-import React, { ReactNode, useState,createContext} from 'react';
+import { LibraryType } from '@/Type/LibraryType';
+import React, { ReactNode, useState,createContext, SetStateAction, Dispatch} from 'react';
 
-export const WorkoutContext= createContext({});
+interface WorkoutContextType {
+  plan: LibraryType[];
+  setPlan: Dispatch<SetStateAction<LibraryType[]>>;
+
+  saved: LibraryType[];
+  setSaved: Dispatch<SetStateAction<LibraryType[]>>;
+
+  sortBy: string;
+  setSortBy: Dispatch<SetStateAction<string>>;
+
+  tab: string;
+  setTab: Dispatch<SetStateAction<string>>;
+};
+
+export const WorkoutContext= createContext<WorkoutContextType>({}as WorkoutContextType);
 
 const WorkoutProvider = ({children}:{children:ReactNode}) => {
-    const[plan,setPlan]=useState([]);
-    const[saved,setSaved]=useState([]);
+    const[plan,setPlan]=useState<LibraryType[]>([]);
+    const[saved,setSaved]=useState<LibraryType[]>([]);
     const [sortBy, setSortBy] = useState('Duration');
     const [tab, setTab] = useState("myPlan");
     const sharedData={
